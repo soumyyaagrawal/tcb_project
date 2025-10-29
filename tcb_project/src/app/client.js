69ab53f67966cbb8001eafde7a9e022b830d3522
soumyyaagrawal <soumyaagrawal2k6@gmail.com>
2025-10-29 19:41:18 +0530

    css

--- tcb_project/src/app/client.js
+++ tcb_project/src/app/client.js
@@ -18,12 +18,17 @@ export default function Client({ articles }) {
 
 
   return (
-    <main className="min-h-screen p-10 bg-gray-100">
-      <h1 className="text-black 3xl font-bold mb-6">InsightDigest</h1>
+
+    <main className="min-h-screen p-10 bg-black">
+      <h1 className="text-purple-700 text-3xl font-bold mb-6">
+        <span className="bg-gradient-to-r from-purple-700  via-purple-500 to-purple-400 text-transparent bg-clip-text">
+          InsightDigest
+        </span>
+      </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {articles.map(article => (
-          <div className="bg-gray-200 shadow p-4 rounded-xl">
+          <div className="bg-gradient-to-t from-gray-800 via-blue-800 to-blue-900 shadow p-4 rounded-xl">
             <img
               src={article.imageUrl}
               alt={article.title}
@@ -38,12 +43,13 @@ export default function Client({ articles }) {
             >
               Read full 
             </a> 
-            <div onClick={() => handleSummarize(article.content || article.description || article.title)} className="text-gray-500 text-sm mt-2 bg-gray-200 shadow p-1 rounded-xl">
+            <div onClick={() => handleSummarize(article.content || article.description || article.title)} className="text-gray-900 text-md text-center mt-2 p-2 bg-gray-200 shadow p-1 rounded-xl">
              summarize  
             </div>
           </div>
         ))}
       </div>
+
     </main>
   )
 }
